ec8a4531fdcd52bcb2f32323969373f020371eaf
KieranWardle <kieranwardle95@gmail.com>
2020-04-09 10:55:42 +0100

    Added listener and event to trigger report

--- src/main/java/uk/gov/ons/fsdr/adeccomock/AdeccoMock.java
+++ src/main/java/uk/gov/ons/fsdr/adeccomock/AdeccoMock.java
@@ -2,9 +2,13 @@ package uk.gov.ons.fsdr.adeccomock;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"uk.gov.ons.census.fwmt.events", "uk.gov.ons.fsdr"})
 public class AdeccoMock {
+
+  public static final String APPLICATION_NAME = "Fsdr Mock";
 
   public static void main(String[] args) {
     SpringApplication.run(AdeccoMock.class, args);
