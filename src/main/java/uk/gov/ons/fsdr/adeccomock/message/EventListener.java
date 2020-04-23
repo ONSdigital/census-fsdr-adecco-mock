367575153782ba8a93e022040fbbcfa3c2a57467
Simon <sdiaz1000@gmail.com>
2020-04-23 08:35:58 +0100

    added a temporary fix for a snow issue

--- src/main/java/uk/gov/ons/fsdr/adeccomock/message/EventListener.java
+++ src/main/java/uk/gov/ons/fsdr/adeccomock/message/EventListener.java
@@ -1,18 +1,21 @@
 package uk.gov.ons.fsdr.adeccomock.message;
 
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.amqp.rabbit.annotation.RabbitHandler;
-
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import uk.gov.ons.census.fwmt.events.component.GatewayEventManager;
-import uk.gov.ons.census.fwmt.events.data.GatewayErrorEventDTO;
-import uk.gov.ons.census.fwmt.events.data.GatewayEventDTO;
-
 import static uk.gov.ons.fsdr.adeccomock.configuration.GatewayEventsConfig.FSDR_COMPLETE;
 import static uk.gov.ons.fsdr.adeccomock.configuration.QueueConfig.EVENTS_QUEUE;
 import static uk.gov.ons.fsdr.adeccomock.service.AdeccoMockService.totalCount;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import lombok.extern.slf4j.Slf4j;
+import uk.gov.ons.census.fwmt.events.component.GatewayEventManager;
+import uk.gov.ons.census.fwmt.events.data.GatewayErrorEventDTO;
+import uk.gov.ons.census.fwmt.events.data.GatewayEventDTO;
 
 @Service
 @RabbitListener(queues = EVENTS_QUEUE)
@@ -22,27 +25,46 @@ public class EventListener {
   @Autowired
   private GatewayEventManager gatewayEventManager;
 
-  int xma = 0;
-  int snow = 0;
+  private static int xma = 0;
+  private static int snow = 0;
+
+  private Object lock = new Object();
+
+  private String lastSnowId = null;
 
   @RabbitHandler
-  public void recieveMessage(GatewayErrorEventDTO eventDTO) {}
+  public void recieveMessage(GatewayErrorEventDTO eventDTO) {
+  }
 
   @RabbitHandler
   public void recieveMessage(GatewayEventDTO eventDTO) {
+    boolean isUpdated = false;
+    if (totalCount == 0)
+      return;
+    synchronized (lock) {
+      switch (eventDTO.getEventType()) {
+      case "SENDING_XMA_ACTION_RESPONSE":
+        xma++;
+        isUpdated = true;
+        break;
+      case "SENDING_SERVICE_NOW_ACTION_RESPONSE":
+        if (!eventDTO.getCaseId().equals(lastSnowId)) {
+          snow++;
+        }else {
+          log.error("Duplicate event: {}", eventDTO.toString());
+        }
+        isUpdated = true;
+        break;
+      }
+      if (isUpdated) {
+        log.info("xmacounter: {} snowcounter: {} total: {} ", xma, snow, totalCount);
 
-    switch(eventDTO.getEventType()) {
-    case "SENDING_XMA_ACTION_RESPONSE":
-      xma++;
-      break;
-    case "SENDING_SERVICE_NOW_ACTION_RESPONSE":
-      snow++;
-      break;
-    }
-    if(xma == totalCount && snow == totalCount) {
-      gatewayEventManager.triggerEvent("<N/A>", FSDR_COMPLETE);
-      xma = 0;
-      snow = 0;
+      }
+      if (xma == totalCount && snow == totalCount) {
+        gatewayEventManager.triggerEvent("<N/A>", FSDR_COMPLETE);
+        xma = 0;
+        snow = 0;
+      }
     }
   }
 
